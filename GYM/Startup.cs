6827ebcd37f396e705f41fba8dd156c8ee40e76d
Ewa Marciniak <emarciniak78@gmail.com>
2014-05-02 08:35:10 +0100

    The first commit

---
+++ GYM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GYM.Startup))]
+namespace GYM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
